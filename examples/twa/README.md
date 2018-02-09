25d55ba8283ef80d528ac1dbf25c6d025be7bd1c
Justin Provazza <jprov410@gmail.com>
2018-02-09 16:38:34 -0500

    add twa_master.f90

---
+++ examples/twa/README.md
@@ -0,0 +1,6 @@
+# TWA examples
+
+#### The current examples consist of:
+---------------------------------------
+
+* two-state twa reduced density matrix calculation in diabatic basis with harmonic bath and bilinear system-bath coupling
---
+++ mqds/src/twa_master.f90
@@ -0,0 +1,42 @@
+!
+! This is the TWA master subroutine that determines
+!      which TWA-based calculation to perform
+!
+SUBROUTINE twa_master
+  USE input_output
+  USE harmonic_bath
+  USE mapping_variables
+  USE hamiltonians
+  USE random_numbers
+  IMPLICIT NONE
+
+  ! Initialize random seed (with mype argument for MPI)
+  CALL initialize_rn
+
+  ! Allocate arrays and initialize mapping variables
+  CALL initialize_twa_map
+
+  ! Do calculations ins the diabatic basis with harmonic bath
+  IF ( bath .EQ. 'harmonic' ) THEN
+
+     CALL initialize_bath
+
+     IF ( basis .EQ. 'diabatic' ) THEN        
+
+        CALL initialize_hel
+
+        IF ( calculation .EQ. 'redmat' ) CALL calculate_twa_redmat
+
+        CALL finalize_hel
+
+     END IF
+
+     CALL finalize_bath
+
+  END IF
+
+
+  ! Deallocate mapping variables
+  CALL finalize_twa_map
+
+END SUBROUTINE twa_master
